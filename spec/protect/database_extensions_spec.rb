c66071753e09aecdbd23451cb49f7803771dcac3
James Sadler <james@cipherstash.com>
2022-10-13 11:15:01 +1100

    Add spec for removal of custom database type

--- spec/protect/database_extensions_spec.rb
+++ spec/protect/database_extensions_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe ProtectRB::DatabaseExtensions do
       ActiveRecord::Base.connection.execute("create table foo ( email_searchable ore_64_8_v1 )")
     end
   end
+
+  describe "Uninstallation" do
+    it "should make the ore_64_8_1_v1 extension type unavailable in the database" do
+      ProtectRB::DatabaseExtensions.install
+      ProtectRB::DatabaseExtensions.uninstall
+
+      expect {
+        ActiveRecord::Base.connection.execute("create table foo ( email_searchable ore_64_8_v1 )")
+      }.to raise_error ActiveRecord::StatementInvalid
+    end
+  end
 end
