25f2a590e6451915ba02d3400ac74de2afaf863d
fimac <fiona@cipherstash.com>
2022-10-17 15:56:34 +1100

    add type in

--- lib/protect_rb/active_record_extensions/ore_64_8_v1.rb
+++ lib/protect_rb/active_record_extensions/ore_64_8_v1.rb
@@ -1,7 +1,13 @@
 module ProtectRB
   module ActiveRecordExtensions
     class ORE_64_8_V1_Type < ::ActiveRecord::Type::Value
-      # Just a placeholder for now
+      def type
+        :ore_64_8_v1
+      end
+    end
+
+    ActiveSupport.on_load(:active_record) do
+      ActiveRecord::Type.register("ore_64_8_v1", ORE_64_8_V1_Type, override: true, adapter: :postgresql)
     end
 
     class ORE_64_8_V1
