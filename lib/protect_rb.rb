28fe571da2cc15e30b61f9e1579bff104dda7a25
fimac <fiona@cipherstash.com>
2022-10-17 15:57:00 +1100

    add protect rb error type

--- lib/protect_rb.rb
+++ lib/protect_rb.rb
@@ -1,9 +1,9 @@
 require 'active_support'
-require_relative './protect_rb/database_extensions'
 require_relative './protect_rb/active_record_extensions'
+require_relative './protect_rb/database_extensions'
 require_relative './protect_rb/logger'
 require_relative './protect_rb/model'
 
 module ProtectRB
-
+  class Error < StandardError; end
 end
--- lib/protect_rb/model.rb
+++ lib/protect_rb/model.rb
@@ -2,11 +2,10 @@ require "active_support/concern"
 require_relative "./model/dsl"
 require_relative "./model/crud"
 
-module ActiveProtect
+module ProtectRB
   module Model
     extend ActiveSupport::Concern
     include ProtectRB::Model::DSL
     include ProtectRB::Model::CRUD
   end
 end
-
