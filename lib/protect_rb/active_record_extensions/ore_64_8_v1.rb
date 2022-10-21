a9620c2620749a1abf48cf51512c00bbc8e513be
fimac <fiona@cipherstash.com>
2022-10-21 15:00:42 +1100

    add custom type functions to cast serialize and deserialize

--- lib/protect_rb/active_record_extensions/ore_64_8_v1.rb
+++ lib/protect_rb/active_record_extensions/ore_64_8_v1.rb
@@ -1,17 +1,66 @@
+require "base64"
 module ProtectRB
   module ActiveRecordExtensions
     class ORE_64_8_V1_Type < ::ActiveRecord::Type::Value
       def type
         :ore_64_8_v1
       end
+
+      def cast(value)
+        if !value.nil?
+          ORE_64_8_V1.encrypt(value)
+        else
+          # TODO this reveals that the column is set to NULL
+          nil
+        end
+      end
+
+      def deserialize(value)
+        if !value.nil?
+          ORE_64_8_V1.new([value[1..-2]].pack("h*").unpack("C*"))
+        else
+          nil
+        end
+      end
+
+      def serialize(value)
+        if !value.nil?
+          bytes = value.ciphertext.to_s.bytes
+
+          "(\"\\\\x#{bytes.pack("C*").unpack("h*").first}\")"
+        else
+          nil
+        end
+      end
     end
+
 
     ActiveSupport.on_load(:active_record) do
       ActiveRecord::Type.register("ore_64_8_v1", ORE_64_8_V1_Type, override: true, adapter: :postgresql)
     end
 
     class ORE_64_8_V1
-      # Just a placeholder for now
+      attr_reader :ciphertext
+
+      def self.encrypt(term)
+        new(ore.encrypt(term))
+      end
+
+      def initialize(ciphertext)
+        @ciphertext = ciphertext
+      end
+
+      private
+
+      def self.ore
+        # Get something working
+        cs_protect_key = ENV["CS_PROTECT_KEY"]
+        prf_key, prp_key = cs_protect_key.split(".")
+
+        @ore ||= begin
+          ORE::AES128.new([prf_key].pack("H*"), [prp_key].pack("H*"), 64, 8)
+        end
+      end
     end
   end
 end
