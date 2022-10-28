375249a51098a6611bb064802c33b9e26eb104e0
James Sadler <james@cipherstash.com>
2022-10-28 22:24:47 +1100

    Specs for database migrations should run in own DB
    We have a test that tests the installation and removal of database
    extensions.
    
    We have a test that tests a customer can *use* the extensions helper
    in their own migrations.
    
    These need to be isolated from the rest of the specs that depend on a
    global set of applied migrations.

--- spec/protect_rb/database_extensions_spec.rb
+++ spec/protect_rb/database_extensions_spec.rb
@@ -1,11 +1,11 @@
-RSpec.describe ProtectRB::DatabaseExtensions do
+RSpec.describe ProtectRB::DatabaseExtensions, :type => :extensions_test do
   after(:each) do
     ProtectRB::DatabaseExtensions.uninstall rescue nil
     ActiveRecord::Base.connection.execute("drop table if exists foo")
   end
 
   describe "Installation" do
-    it "should make the ore_64_8_1_v1 extension type available in the database" do
+    it "should make the ore_64_8_v1 extension type available in the database" do
       ProtectRB::DatabaseExtensions.install rescue nil
 
       ActiveRecord::Base.connection.execute("create table foo ( email_searchable ore_64_8_v1 )")
@@ -13,7 +13,7 @@ RSpec.describe ProtectRB::DatabaseExtensions do
   end
 
   describe "Uninstallation" do
-    it "should make the ore_64_8_1_v1 extension type unavailable in the database" do
+    it "should make the ore_64_8_v1 extension type unavailable in the database" do
       ProtectRB::DatabaseExtensions.install rescue nil
       ProtectRB::DatabaseExtensions.uninstall rescue nil
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,31 +10,43 @@ require "database_cleaner"
 ENV["LOCKBOX_MASTER_KEY"] = Lockbox.generate_key
 ENV["CS_PROTECT_KEY"] = ProtectRB.generate_key
 
-ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  host: 'localhost',
-  username: ENV["PGUSER"] || nil,
-  password: ENV["PGPASSWORD"] || nil,
-  database: ENV["PGDATABASE"] || 'protect_rb_test'
-)
 
 RSpec.configure do |config|
   config.full_backtrace = ENV.key?("RSPEC_FULL_BACKTRACE")
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-
-    ActiveRecord::MigrationContext.new(File.join("spec", "support", "migrations"), ActiveRecord::SchemaMigration).migrate
-
-    Dir["./spec/support/models/*.rb"].each {|file| require  file }
+  config.around(:each, :type => :extensions_test) do |example|
+    ActiveRecord::Base.establish_connection(
+      adapter: 'postgresql',
+      host: 'localhost',
+      username: ENV["PGUSER"] || nil,
+      password: ENV["PGPASSWORD"] || nil,
+      database: 'protect_rb_test_alt'
+    )
   end
 
   config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
+    if example.metadata[:type] != :extensions_test
+      ActiveRecord::Base.establish_connection(
+        adapter: 'postgresql',
+        host: 'localhost',
+        username: ENV["PGUSER"] || nil,
+        password: ENV["PGPASSWORD"] || nil,
+        database: 'protect_rb_test'
+      )
+
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+
+      ActiveRecord::MigrationContext.new(File.join("spec", "support", "migrations"), ActiveRecord::SchemaMigration).migrate
+
+      Dir["./spec/support/models/*.rb"].each {|file| require  file }
+
+      DatabaseCleaner.cleaning do
+        example.run
+      end
     end
   end
+
 
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
