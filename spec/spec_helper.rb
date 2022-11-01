1439d8afc3deeb4e7731e844acc1da67358850ea
Robin Howard <robin@cipherstash.com>
2022-11-01 17:31:17 +1100

    Optimises the spec_helper a little, and tags tests with :type => :db by default to aid before/around matching.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,44 +10,57 @@ require "database_cleaner"
 ENV["LOCKBOX_MASTER_KEY"] = Lockbox.generate_key
 ENV["CS_PROTECT_KEY"] = ProtectRB.generate_key
 
+def establish_connection(**attrs)
+  ActiveRecord::Base.establish_connection(
+    {
+      adapter: 'postgresql',
+      host: ENV["PGHOST"] || 'localhost',
+      username: ENV["PGUSER"] || nil,
+      password: ENV["PGPASSWORD"] || nil,
+    }.merge(attrs)
+  )
+end
 
 RSpec.configure do |config|
   config.full_backtrace = ENV.key?("RSPEC_FULL_BACKTRACE")
 
-  config.around(:each, :type => :extensions_test) do |example|
-    ActiveRecord::Base.establish_connection(
-      adapter: 'postgresql',
-      host: 'localhost',
-      username: ENV["PGUSER"] || nil,
-      password: ENV["PGPASSWORD"] || nil,
+  # Everything in spec/protect_rb/ is a :type => :db spec unless marked
+  # otherwise, eg.
+  #   RSpec.describe "tests", type: :extensions_test do
+  config.define_derived_metadata(
+    file_path: Regexp.new('/spec/protect_rb/')
+  ) do |metadata|
+    metadata[:type] = :db if metadata[:type].nil?
+  end
+
+  ### Extension-related tests ###
+  config.before(:all, type: :extensions_test) do
+    establish_connection(
       database: 'protect_rb_test_alt'
     )
   end
 
-  config.around(:each) do |example|
-    if example.metadata[:type] != :extensions_test
-      ActiveRecord::Base.establish_connection(
-        adapter: 'postgresql',
-        host: 'localhost',
-        username: ENV["PGUSER"] || nil,
-        password: ENV["PGPASSWORD"] || nil,
-        database: 'protect_rb_test'
-      )
+  ### Regular :db tests ###
+  config.before(:all, type: :db) do
+    establish_connection(
+      database: 'protect_rb_test'
+    )
 
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
 
-      ActiveRecord::MigrationContext.new(File.join("spec", "support", "migrations"), ActiveRecord::SchemaMigration).migrate
+    ActiveRecord::MigrationContext.new(File.join("spec", "support", "migrations"), ActiveRecord::SchemaMigration).migrate
 
-      Dir["./spec/support/models/*.rb"].each {|file| require  file }
+    Dir["./spec/support/models/*.rb"].each {|file| require  file }
+  end
 
-      DatabaseCleaner.cleaning do
-        example.run
-      end
+  config.around(:each, type: :db) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
     end
   end
 
-
+  ### Misc configuration ###
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
