3334c5a4e716cd339ced0ef28f0ee9c975e095c1
James Sadler <james@cipherstash.com>
2022-11-07 17:08:49 +1100

    Subsume lockbox DSL
    `secure_search` now accepts a `type` parameter (defaulting to :string)
    if not supplied.
    
    `has_encrypted` is now called automatically by `secure_search`.
    
    This means that all `has_encrypted` calls can be omitted from models.

--- spec/support/models/crud_testing.rb
+++ spec/support/models/crud_testing.rb
@@ -1,17 +1,10 @@
 class CrudTesting < ActiveRecord::Base
   self.table_name = "users_for_crud_testing"
 
-  has_encrypted :email
-  has_encrypted :dob, type: :date
-  has_encrypted :last_login, type: :datetime
-  has_encrypted :age, type: :integer
-  has_encrypted :verified, type: :boolean
-  has_encrypted :latitude, type: :float
-
   secure_search :email
-  secure_search :dob
-  secure_search :last_login
-  secure_search :age
-  secure_search :verified
-  secure_search :latitude
+  secure_search :dob, type: :date
+  secure_search :last_login, type: :datetime
+  secure_search :age, type: :integer
+  secure_search :verified, type: :boolean
+  secure_search :latitude, type: :float
 end
--- spec/support/models/plaintext_testing.rb
+++ spec/support/models/plaintext_testing.rb
@@ -1,15 +1,8 @@
 class PlaintextTesting < ActiveRecord::Base
   self.table_name = "plaintext_users"
 
-
-  has_encrypted :dob, type: :date
-  has_encrypted :last_login, type: :datetime
-  has_encrypted :verified, type: :boolean
-  has_encrypted :latitude, type: :float
-
-
-  secure_search :dob
-  secure_search :last_login
-  secure_search :verified
-  secure_search :latitude
+  secure_search :dob, type: :date
+  secure_search :last_login, type: :datetime
+  secure_search :verified, type: :boolean
+  secure_search :latitude, type: :float
 end
