ba10f22730f5fe95ae44418bf1b1ad4cf8250236
James Sadler <james@cipherstash.com>
2022-11-08 16:12:10 +1100

    Fix log message to prefix with "Protect" not ProtectRB"

--- lib/protect/logger.rb
+++ lib/protect/logger.rb
@@ -6,15 +6,15 @@ module Protect
       end
 
       def info(message)
-        instance.info("  \e[36m\e[1mProtectRB\e[22m: #{message}\e[0m")
+        instance.info("  \e[36m\e[1mProtect\e[22m: #{message}\e[0m")
       end
 
       def warn(message)
-        instance.warn("  \e[33m\e[1mProtectRB WARNING\e[22m: #{message}\e[0m")
+        instance.warn("  \e[33m\e[1mProtect WARNING\e[22m: #{message}\e[0m")
       end
 
       def error(message)
-        instance.error("  \e[31m\e[1mProtectRB ERROR\e[22m: #{message}\e[0m")
+        instance.error("  \e[31m\e[1mProtect ERROR\e[22m: #{message}\e[0m")
       end
 
       def instance
