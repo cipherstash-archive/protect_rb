44be112625a6d9bba233c9004cec6cac1469ef5f
fimac <fiona@cipherstash.com>
2022-11-10 11:18:23 +1100

    add rake task

---
+++ lib/protect/railtie.rb
@@ -0,0 +1,10 @@
+require 'rails'
+
+module Protect
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      path = File.expand_path(File.join(__dir__, "../tasks/protect.rake"))
+      load path
+    end
+  end
+end
