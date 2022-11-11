a9baa940ea285aa59f785e04efc672af912b4d40
fimac <fiona@cipherstash.com>
2022-11-11 15:41:43 +1100

    revert back to using inline model and add a model name to prevent error

--- spec/support/models/uniqueness_validations.rb
+++
@@ -1,6 +0,0 @@
-class UniquenessTesting < ActiveRecord::Base
-  self.table_name = "users_for_uniqueness_testing"
-
-  secure_search :example_validation, type: :string
-  validates :example_validation, uniqueness: true
-end
