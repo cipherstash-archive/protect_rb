4ccfcf64b8f59a68eb1c4d1206f7a2c2274082e7
fimac <fiona@cipherstash.com>
2022-11-11 15:17:52 +1100

    ore encrypt plaintext and pass as value to check for uniqueness

--- lib/protect/active_record_extensions/uniqueness_validator.rb
+++ lib/protect/active_record_extensions/uniqueness_validator.rb
@@ -4,7 +4,8 @@ module Protect
       def validate_each(record, attribute, value)
         protect_attr = record.class.protect_search_attrs[attribute.to_sym]
         if protect_attr
-          value = record.read_attribute_for_validation(protect_attr[:searchable_attribute])
+          virt_attr_val = record.read_attribute_for_validation(protect_attr[:lockbox_attribute][:attribute])
+          value = Protect::ActiveRecordExtensions::ORE_64_8_V1.encrypt(virt_attr_val)
         end
         super(record, attribute, value)
       end
