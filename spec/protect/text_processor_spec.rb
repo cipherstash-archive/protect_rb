10c42052943a5fa1fa1436da9e830ebccca2ba54
fimac <fiona@cipherstash.com>
2022-12-01 14:11:48 +1100

    update text analysis settings to use symbols

--- spec/protect/text_processor_spec.rb
+++ spec/protect/text_processor_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Protect::Analysis::TextProcessor do
       tokenizer =
         Protect::Analysis::TextProcessor.new({
           token_filters:[
-            { kind: "downcase" }
+            { kind: :downcase }
           ],
-         tokenizer: { kind: "standard" }
+         tokenizer: { kind: :standard }
         })
       result = tokenizer.perform("This is an example of a standard tokenizer")
       expect(result.length).to eq(8)
@@ -19,7 +19,7 @@ RSpec.describe Protect::Analysis::TextProcessor do
        expect {
           Protect::Analysis::TextProcessor.new({
             token_filters:[
-              { kind: "downcase" }
+              { kind: :downcase }
             ]
           })
         }.to raise_error(Protect::Error, "No tokenizer provided. Use tokenizer: {kind: 'standard'} in your settings.")
@@ -29,7 +29,7 @@ RSpec.describe Protect::Analysis::TextProcessor do
         expect {
           Protect::Analysis::TextProcessor.new({
             token_filters:[
-              { kind: "downcase" }
+              { kind: :downcase }
             ],
             tokenizer: { kind: "non-standard" }
           })
@@ -39,7 +39,7 @@ RSpec.describe Protect::Analysis::TextProcessor do
     it "raises an error if no token filters are provided" do
       expect {
          Protect::Analysis::TextProcessor.new({
-         tokenizer: { kind: "standard" }
+         tokenizer: { kind: :standard }
         })
          }.to raise_error(Protect::Error, "No token filters provided.")
     end
@@ -50,10 +50,10 @@ RSpec.describe Protect::Analysis::TextProcessor do
        tokenizer =
           Protect::Analysis::TextProcessor.new({
             token_filters:[
-              {kind: "downcase"},
-              {kind: "ngram", token_length: 3}
+              {kind: :downcase},
+              {kind: :ngram, token_length: 3}
             ],
-           tokenizer: { kind: "standard" }
+           tokenizer: { kind: :standard }
           })
         result = tokenizer.perform("This is an example of an ngram filter")
         expect(result).to eq(["thi", "his", "exa", "xam", "amp", "mpl", "ple", "ngr", "gra", "ram", "fil", "ilt", "lte", "ter"])
@@ -63,10 +63,10 @@ RSpec.describe Protect::Analysis::TextProcessor do
       expect {
         Protect::Analysis::TextProcessor.new({
           token_filters:[
-            {kind: "downcase"},
-            {kind: "ngram"}
+            {kind: :downcase},
+            {kind: :ngram}
           ],
-         tokenizer: { kind: "standard" }
+         tokenizer: { kind: :standard }
         })
         }.to raise_error(Protect::Error, "Token length not provided. Please specify token length using '{kind: 'ngram', tokenLength: 3}'")
     end
