564792cd3e7e68834d122a6c084ef8c8fae0bef1
Bennett Hardwick <bennett@cipherstash.com>
2022-12-20 09:33:25 +1000

    Init rutie

---
+++ ciphertext_sdk_test/lib/ciphertext_sdk_test.rb
@@ -0,0 +1,6 @@
+require "ciphertext_sdk_test/version"
+
+module CiphertextSdkTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ ciphertext_sdk_test/lib/ciphertext_sdk_test/version.rb
@@ -0,0 +1,3 @@
+module CiphertextSdkTest
+  VERSION = "0.1.0"
+end
